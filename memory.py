ed8dd9fe51edbf4a4c989e38db3e0702d45eef2a
Leon Soler <laaaion@pop-os.localdomain>
2022-04-13 19:54:35 -0300

    signal fix

--- memory.py
+++ memory.py
@@ -5,52 +5,22 @@ from amaranth.sim import *
 from enum import Enum, unique
 
 class MEMORY(Elaboratable):
-    def __init__(self):
-        self.addr = Signal(32)
-        self.dataIn = Signal(32)
-        self.rdStb = Signal()
-        self.wrStb = Signal()
-        self.dataOut = Signal(32)
-        self.mem = Array([Signal(32) for _ in range(1024)])
-
-
-
-    def ports(self):
-        return[self.addr, self.dataIn, self.rdStb, self.wrStb,self.dataOut]
+    def __init__(self, Addr: Signal(32), DataIn: Signal(32), RdStb: Signal(), WrStb: Signal(), DataOut: Signal(32),mem : Array([Signal(32) for _ in range(132)])):
+        self.Addr = Addr
+        self.DataIn = DataIn
+        self.RdStb = RdStb
+        self.WrStb = WrStb
+        self.DataOut = DataOut
+        #memoria de 1024 palabras de 32 bits
+        self.mem = mem
     
 
     def elaborate(self, platform):
         m = Module()
-        pos = ClockDomain("pos")
-        neg = ClockDomain("neg", clk_edge="neg")
-        neg.clk = pos.clk
-        neg.rst = pos.rst
-        m.domains += [pos,neg]
             
-    
-        with m.If(self.wrStb):
-            m.d.neg += self.dataIn.eq(self.mem[self.addr])
-        with m.If(self.rdStb):
-            m.d.neg += self.dataOut.eq(self.mem[self.addr])
+        with m.If(self.WrStb):
+            m.d.neg += self.mem[self.Addr].eq(self.DataIn)
+        with m.Elif(self.RdStb):
+            m.d.neg += self.DataOut.eq(self.mem[self.Addr])
              
-
         return m
-
-def proc():
-    address = Signal(32)
-    with open("program1") as file:
-        for line in file:
-            yield dut.mem[address].eq(int(line.rstrip(),16))
-            yield address.eq(address+4)
-            yield Settle()
-    yield Delay(2e-8)
-    yield dut.addr.eq(8)
-    yield dut.rdStb.eq(1)
-    yield Delay(2e-8)
-    
-dut = MEMORY()
-sim = Simulator(dut)
-sim.add_clock(2e-8,domain="neg")
-sim.add_process(proc)
-with sim.write_vcd("memory.vcd"):
-    sim.run()
--- registers.py
+++ registers.py
@@ -6,29 +6,22 @@ from enum import Enum, unique
 from amaranth.lib.coding import Encoder
 
 class REGISTERS(Elaboratable):
-    def __init__(self):
-        self.reset = Signal()
-        self.wr = Signal()
-        self.reg1_rd = Signal(5)
-        self.reg2_rd = Signal(5)
-        self.reg_wr = Signal(5)
-        self.data_wr = Signal(32)
-        self.data1_rd = Signal(32)
-        self.data2_rd = Signal(32)
+    def __init__(self,wr: Signal(), reset: Signal(), reg1_rd : Signal(5),reg2_rd: Signal(5),reg_wr: Signal(5), data_wr: Signal(32), data1_rd : Signal(32), data2_rd: Signal(32)):
+        self.reset = reset
+        self.wr = wr
+        self.reg1_rd = reg1_rd
+        self.reg2_rd = reg2_rd
+        self.reg_wr = reg_wr
+        self.data_wr = data_wr
+        self.data1_rd = data1_rd
+        self.data2_rd = data2_rd
+        
         self.mem = Array([Signal(32) for _ in range(32)])
 
     
-    def ports(self):
-        return [self.wr,self.reg1_rd,self.reg2_rd,self.reg_wr,self.data1_rd,self.data2_rd,self.reset]
     
     def elaborate(self, platform):
         m = Module()
-        pos = ClockDomain("pos")
-        neg = ClockDomain("neg", clk_edge="neg")
-
-        neg.clk = pos.clk
-        neg.rst = pos.rst
-        m.domains += [pos,neg]
         
         m.d.comb += self.data1_rd.eq(Mux(self.reg1_rd==0, 0, self.mem[self.reg1_rd]))
         m.d.comb += self.data2_rd.eq(Mux(self.reg2_rd==0, 0, self.mem[self.reg2_rd]))
@@ -43,37 +36,8 @@ class REGISTERS(Elaboratable):
 
         return m
 
-def proc():
-
-    yield dut.wr.eq(1)
-    yield Delay(2e-8)
-    for i in range(32):
-        yield Delay(2e-8)
-        yield dut.reg_wr.eq(i)
-        yield dut.data_wr.eq(i)
-        yield Delay(2e-8)
-    yield Delay(2e-8)
-    yield dut.wr.eq(0)
-    for i in range(32):
-        yield dut.reg1_rd.eq(32-i)
-        yield dut.reg2_rd.eq(i)
-        yield Delay(2e-8)
-    yield dut.reset.eq(1)
-    for i in range(32):
-        yield dut.reg1_rd.eq(32-i)
-        yield dut.reg2_rd.eq(i)
-        yield Delay(2e-8)
 
     
     
-    
 
-dut = REGISTERS()
-sim = Simulator(dut)
-sim.add_clock(2e-8,domain="neg")
-sim.add_process(proc)
-sim.reset()
-
-with sim.write_vcd("registers.vcd"):
-    sim.run()
         
